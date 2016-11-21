e35c89a77b2123c07f9f289fe2c50da32b7dbf20
iorfix <jason_di@yahoo.it>
2016-11-21 10:27:56 +0100

    demo4

--- demo/chaincode_demo.go
+++ demo/chaincode_demo.go
@@ -103,8 +103,11 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "read" { //read a variable
-		return t.read(stub, args)
+	if function == "readWaste" { //read a variable
+		waste, err := t.readWaste(stub, args)
+		if err != nil { return nil, errors.New("Error retrieving waste information")}
+		return json.Marshal(waste)
+		 
 	}
 	fmt.Println("query did not find func: " + function)
 
@@ -166,20 +169,23 @@ func (t *SimpleChaincode) get_username(stub shim.ChaincodeStubInterface) (string
 
 
 // read - query function to read key/value pair
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-//	var key, jsonResp string
-//	var err error
-	return nil, errors.New("Non aoncora implementata")
-//	if len(args) != 1 {
-//		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-//	}
+func (t *SimpleChaincode) readWaste(stub shim.ChaincodeStubInterface, args []string) (Waste, error) {
+	var key, jsonResp string
+	var waste Waste
+	
+	if len(args) != 1 {
+		return waste, errors.New("Incorrect number of arguments. Expecting name of the Waste id")
+	}
 
-//	key = args[0]
-//	valAsbytes, err := stub.GetState(key)
-//	if err != nil {
-//		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-//		return nil, errors.New(jsonResp)
-//	}
-
-//	return valAsbytes, nil
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return waste, errors.New(jsonResp)
+	}
+	err = json.Unmarshal(valAsbytes, &waste);
+    if err != nil {	
+		fmt.Printf("retrieve WASTE: Corrupt Waste "+string(valAsbytes)+": %s", err) 
+		return waste, errors.New("RETRIEVE_WASTE: Corrupt waste record"+string(valAsbytes))	}
+	return waste, nil
 }
